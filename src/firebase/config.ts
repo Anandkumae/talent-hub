e634e5971d8d41b702116610675be1130ea9b858
Anand <ramashankarsingh841@gmail.com>
2025-11-03 07:01:06 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1019997559022:web:4eb16cb5d67fe3346bbbff",
   "apiKey": "AIzaSyAw6SU86ZRqNdLCbIqFOkaIkbVf0SKX_3Q",
   "authDomain": "studio-876884497-58164.firebaseapp.com",
+  "storageBucket": "studio-876884497-58164.appspot.com",
   "measurementId": "",
   "messagingSenderId": "1019997559022"
 };
